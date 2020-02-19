32505d835fcfc07af2fb3b1e69896797de224af6
tianxiang84 <tsu@slb.com>
2020-02-18 23:25:45 -0500

    working svd

--- java/my-first-java-app/src/myfirstapp/MyJavaApp.java
+++
@@ -1,7 +0,0 @@
-package myfirstapp;
-
-public class MyJavaApp {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
--- java/svd_saved/src/svd/SVD.java
+++
@@ -1,7 +0,0 @@
-package svd
-
-public class SVD {
-    public static void main(String[] args){
-        System.out.println("test");
-    }
-}
