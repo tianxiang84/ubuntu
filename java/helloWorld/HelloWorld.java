9ab63e6790be6785adb24cbcc0bf8c38166636f4
tianxiang84 <tsu@slb.com>
2020-02-18 23:29:02 -0500

    added back first 2 test projects

---
+++ java/helloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ *  Application entry point
+ *
+ *  @param args array of command-line arguments passed to this method
+ */
+ 
+class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      System.out.println("hello, world. I read " + args[0]);
+   }
+}
