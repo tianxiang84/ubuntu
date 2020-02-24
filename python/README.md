79e2c7c6aab908d7827007edae6691ad71e543e0
Tianxiang Su from Dell 5510 <TSu@slb.com>
2020-02-24 10:45:45 -0500

    Python GMM import

---
+++ python/README.md
@@ -0,0 +1,3 @@
+Go to python/docker
+Review the docker-compose.yml file (mounting dir and work_dir)
+Run docker-compose run python3p8
---
+++ python/src/gmm_cluster/gmm_cluster.py
@@ -0,0 +1,17 @@
+"""
+This is a Python script for GMM exercise.
+Ref: https://jakevdp.github.io/PythonDataScienceHandbook/05.12-gaussian-mixtures.html
+su.tianxiang at gmail
+Feb 2020
+"""
+
+import sys
+print(sys.version)
+import os
+
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+
+print('I am running in a container but printing in host.')
